4d581b817b873c32c3cdb9dbc75c9dcbe0909097
alfred <129723203+alfred04joseph@users.noreply.github.com>
2023-06-09 20:45:40 -0600

    Create objetos.js

---
+++ fundaPrograma/objetos.js
@@ -0,0 +1,23 @@
+
+let album = {
+    genero: " Rock ",
+    nameMusic: " Whispers In The Dark ",
+    2: 1995
+    };
+    //console.log(album[genero]);
+    //console.log(album["nameMusic"]);
+   //console.log(album[years]);
+   let uno=album[2];
+   
+
+
+
+    
+
+
+    
+    
+    
+    
+    
+    
